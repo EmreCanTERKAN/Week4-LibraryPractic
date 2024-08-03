57bbc5b8321d05d24692f0a48b743835977caf97
EmreCanTERKAN <emrecanterkan06@gmail.com>
2024-08-03 12:31:03 +0300

    Add project files.

---
+++ Week4-LibraryPractic/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+class Program
+{
+
+    static void Main(string[] args)
+    {
+        //Create new book
+        Library book1 = new ()
+        {
+            BookName = "Kürk Mantolu Madonna",
+            AuthorName = "Sabahattin",
+            AuthorLastName = "Ali",
+            PagesNumber = 177,
+            Publisher = "Yapı Kredi Yayınları"
+        };
+        Console.WriteLine($" Book Name :  {book1.BookName}\n Author Name : {book1.AuthorName}\n Author Last Name: {book1.AuthorLastName}\n Pages Number : {book1.PagesNumber}\n Publisher Name : {book1.Publisher}\n");
+
+        // Create Second Book
+        Library book2 = new ("Kendime Düşünceler","Marcus","Aurelius",156, "İş Bankası Kültür Yayınları");
+        Console.WriteLine($" Book Name :  {book2.BookName}\n Author Name : {book2.AuthorName}\n Author Last Name: {book2.AuthorLastName}\n Pages Number : {book2.PagesNumber}\n Publisher Name : {book2.Publisher}\n");
+    }
+}
+
+// Libary class
+class Library
+{
+    public string BookName { get; set; }
+    public string AuthorName { get; set; }
+    public string AuthorLastName { get; set; }   
+    public int PagesNumber { get; set; }
+    public string Publisher { get; set; }
+    public DateTime RegistrationDate { get; set; }
+
+    //Default Constructor
+    public Library()
+    {
+        Console.WriteLine($" Date Of Registration : {DateTime.Now}");
+    }
+
+//Params Constructor
+    public Library(string bookname, string authorname, string authorlastname,int pagenumber , string publisher ) :this()
+    {
+        BookName = bookname;
+        AuthorName = authorname;
+        AuthorLastName = authorlastname;
+        Publisher = publisher;
+        PagesNumber = pagenumber;
+    }
+
+
+}
